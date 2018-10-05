402c6c838279e17fc355609759be907eb9fa8027
Fatih Santalu <ftsantalu@gmail.com>
2018-10-05 16:35:05 +0300

    Update dependencies

--- app/src/main/java/com/santalu/myapplication/Extensions.kt
+++ app/src/main/java/com/santalu/myapplication/Extensions.kt
@@ -10,10 +10,10 @@ import android.widget.Toast
  * Created by fatih.santalu on 7/24/2018.
  */
 
-fun ViewGroup.inflate(layoutRes: Int): View {
+internal fun ViewGroup.inflate(layoutRes: Int): View {
   return LayoutInflater.from(context).inflate(layoutRes, this, false)
 }
 
-fun Context.toast(text: CharSequence) {
+internal fun Context.toast(text: CharSequence) {
   Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
 }
